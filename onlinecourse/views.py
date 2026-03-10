d0fdd036e8aa710bdc7f30b0b757588fb13da563
Ayush Gupta <119103996+AYUSH0613@users.noreply.github.com>
2026-03-10 15:53:53 +0530

    Update views.py

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
+from .models import Course, Submission, Choice
 
 def submit(request, course_id):
-    return render(request, "exam_result.html")
+
+    course = Course.objects.get(id=course_id)
+
+    selected_choices = request.POST.getlist('choice')
+
+    submission = Submission.objects.create()
+
+    for choice_id in selected_choices:
+        choice = Choice.objects.get(id=choice_id)
+        submission.choices.add(choice)
+
+    return render(request, 'exam_result.html', {'course': course})
+
 
 def show_exam_result(request, course_id):
-    return render(request, "exam_result.html")
+
+    course = Course.objects.get(id=course_id)
+
+    submissions = Submission.objects.all()
+
+    score = 0
+
+    for submission in submissions:
+        for choice in submission.choices.all():
+            if choice.is_correct:
+                score += 1
+
+    context = {
+        'course': course,
+        'score': score
+    }
+
+    return render(request, 'exam_result.html', context)
