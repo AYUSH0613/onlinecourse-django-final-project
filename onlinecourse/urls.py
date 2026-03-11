963e1b935a5f9f157df8bf12241ffd52e66b1d3b
Ayush Gupta <119103996+AYUSH0613@users.noreply.github.com>
2026-03-11 15:23:30 +0530

    Update urls.py

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -3,8 +3,12 @@ from . import views
 
 urlpatterns = [
 
-    path('submit/<int:course_id>/', views.submit, name='submit'),
+    path('<int:course_id>/submit/',
+         views.submit,
+         name='submit'),
 
-    path('exam_result/<int:course_id>/', views.show_exam_result, name='show_exam_result'),
+    path('exam_result/<int:course_id>/',
+         views.show_exam_result,
+         name='show_exam_result'),
 
 ]
