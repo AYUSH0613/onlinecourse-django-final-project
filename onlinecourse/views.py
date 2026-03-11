f8d7159925d4c565de7081adc253d4479bd8de69
Ayush Gupta <119103996+AYUSH0613@users.noreply.github.com>
2026-03-11 15:23:13 +0530

    Update views.py

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,37 +1,45 @@
-from django.shortcuts import render
-from .models import Course, Submission, Choice
+from django.shortcuts import render, get_object_or_404
+from .models import Course, Enrollment, Submission, Choice, Question
+
 
 def submit(request, course_id):
 
-    course = Course.objects.get(id=course_id)
+    course = get_object_or_404(Course, pk=course_id)
+    enrollment = Enrollment.objects.get(user=request.user, course=course)
 
     selected_choices = request.POST.getlist('choice')
 
-    submission = Submission.objects.create()
+    submission = Submission.objects.create(enrollment=enrollment)
 
     for choice_id in selected_choices:
         choice = Choice.objects.get(id=choice_id)
         submission.choices.add(choice)
 
-    return render(request, 'exam_result.html', {'course': course})
+    return show_exam_result(request, course_id)
 
 
 def show_exam_result(request, course_id):
 
-    course = Course.objects.get(id=course_id)
+    course = get_object_or_404(Course, pk=course_id)
+    enrollment = Enrollment.objects.get(user=request.user, course=course)
+    submission = Submission.objects.filter(enrollment=enrollment).last()
 
-    submissions = Submission.objects.all()
+    questions = Question.objects.filter(lesson__course=course)
 
-    score = 0
+    total_score = 0
+    possible_score = len(questions)
 
-    for submission in submissions:
-        for choice in submission.choices.all():
-            if choice.is_correct:
-                score += 1
+    for question in questions:
+        if submission.is_get_score(question):
+            total_score += 1
+
+    grade = (total_score / possible_score) * 100
 
     context = {
         'course': course,
-        'score': score
+        'grade': grade,
+        'possible': possible_score,
+        'score': total_score
     }
 
-    return render(request, 'exam_result.html', context)
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
